dc777a40204c0bc2e4ef6b844711fe66f54fa41f
Navneet Kumar <navneetnaagpal@gmail.com>
2016-07-14 11:12:39 -0700

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,7 @@ gulp.task('clean', function(cb) {
 
 gulp.task('test', function() {
     return karma.start({
-        configFile: __dirname + '\\karma.conf.js'
+        configFile: __dirname + '/karma.conf.js'
     });
 });
 
